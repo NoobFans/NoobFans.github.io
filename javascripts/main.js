af7c0b4ab36f8f29990719ad7713ebe5bfddb76f
NoobFans <56683464@qq.com>
2016-11-24 17:36:03 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+	var allLi = document.getElementsByTagName("li");
+	for (var i=0; i<allLi.length; i++){
+		allLi[i].onmouseover = function() {
+		this.getElementsByClassName('top_l2_nav')[0].style.overflow = "visible";
+		}
+		allLi[i].onmouseout = function() {
+		this.getElementsByClassName('top_l2_nav')[0].style.overflow = "hidden";
+		}
+	}
+}
+	
